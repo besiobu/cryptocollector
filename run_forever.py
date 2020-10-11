040d5e6ee69ae8efb1f399a1a18c8898bbd9995c
besiobu <brian.esiobu@gmail.com>
2020-10-11 15:21:00 +0200

    Fix KeybordInterrupt disconnecting.

--- run_forever.py
+++ run_forever.py
@@ -1,44 +1,65 @@
 import sys
+import time 
 
-from time import sleep
 from cryptocollector.exchange.feeds import Feeds
 from cryptocollector.utils.utils import setup_logger
 
 logger = setup_logger(name=__name__)
 
 def run_forever(exchange_name, delay=2):
+    """
+
+    Keep connecting to exchange websocket.
+
+    """
 
     def run_once():
+        """
 
+        Connect to exchange websocket.
+
+        """
         feed = Feeds()
 
-        # Get exchange feed.
         try:
+            # Get exchange feed.
             exchange = getattr(feed, exchange_name)()
         except Exception:
             print(f'{sys.argv[1]} is not supported.')
             return
 
-        # Run until exchange feed stops.
         exchange.daemon = True
         exchange.start()
 
-        while not exchange.stop.is_set():
-            sleep(2)        
-        else:
+        try:            
+            # Run until exchange feed stops.
+            while not exchange.stop.is_set():
+                time.sleep(delay)        
+            else:
+                if exchange.monitor is not None:
+                    exchange.monitor.join()
+                exchange.join()                
+        except KeyboardInterrupt as e:            
+
+            logger.critical(repr(e))            
+
+            # Stop threads and close db connection
+            exchange.stop.set()
             if exchange.monitor is not None:
                 exchange.monitor.join()
-            exchange.join()
+            exchange.join()         
+
+            return True
 
     stop = False
     while not stop:
         try:
             logger.info('Started.')
-            run_once()
+            stop = run_once()
         except KeyboardInterrupt as e:
             logger.critical(repr(e))
             stop = True
-        sleep(2)
+        time.sleep(delay)
     logger.info('Stopped.')            
 
 if __name__ == '__main__':
